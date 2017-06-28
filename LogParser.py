a89c994df9947f7351fee9a973ea96c321fa00c2
jkguiang <jkguiang@gmail.com>
2017-06-28 13:03:42 -0700

    updated logparser, added functionality to plotter, uploaded plots

--- LogParser.py
+++ LogParser.py
@@ -13,57 +13,27 @@ class log_file(dict):
             return False
         else:
             return True
+
+#Checks that line contains column values (columns are only composed of commas and numbers)
+def is_col(line):
+    badChar = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+    for char in line:
+        if char in badChar:
+            return False
+
+    return True
         
-
-#Take line, splits it at "junk", and returns desired string - too much variance in where split occurs for this function to be valuable
-#def getLine(line, junk):
-#    trim = line.split(junk)
-#    if trim[0] == junk:
-#        return trim[1]
-#    else:
-#        return trim[0]
-
-#Takes "month-day hours:minutes:seconds" returns unix UTC time stamp - unneccesary as long as epoch unix time is provided
-#def getTime(line):
-#    if len(line) < 18:
-#        line = (str(datetime.date.today().year)+"-"+line)
-#    return time.mktime(datetime.datetime.strptime(line, "%Y-%d-%m %H:%M:%S").timetuple())
-
-#Takes list containing "head groups" (groupings made by dstat) returns dictionary hgDict -> headGroup:[header1, header2, ...]
-#def mkHGDict(hGroups, headers):
-#    hgDict = {}
-#    counter = 0
-#    hGroups.append(',')
-#    Mhead = hGroups[0]
-#    for i in hGroups:
-#        if i == '':
-#            continue
-#        elif i == "\n":
-#            continue
-#        elif ',' in i:
-#            for j in i:
-#                hgDict[Mhead].append(headers[counter])
-#                counter += 1
-#        else:
-#            trim = i.split("\"")
-#            Mhead = trim[0]
-#            hgDict[Mhead] = []
-#    return hgDict
-
 #Takes logfile directory addres as string, parses logfiles, organizes information in dictionary, passes information into a log_file instance
 def log_parser(logName):
     logDict = {}
     headers = []
     headGroups = []
     
-    if logName.endswith('.txt'):
+    if logName.endswith('.out'):
         with open(logName, 'r') as curfile:
-            next(curfile)
-
             for line in curfile:
-                
                 #Looking for other important info
-                if ",," in line:
+                if "Host:" in line:
                     if "Host" in line:
                         trim = line.split(",") #getLine
                         logDict["Host"] = trim[1]
@@ -75,21 +45,21 @@ def log_parser(logName):
                     if splitLine[0] == "\n":
                         continue
                     #Headers
-                    elif "\"" in splitLine[0]:
+                    elif "usr" in splitLine[0]:
                         for i in splitLine:
                             trim = i.split("\"") #getLine
                             headers.append(trim[1])
                         for i in headers:
                             logDict[i] = []
                     #Column info
-                    else:
+                    elif is_col(line):
                         counter = 0
                         for i in splitLine:
                             #Non-unix Time Stamp
                             if " " in i:
                                 counter += 1
                                 continue #non-unix time stamp not used, because dstat provides epoch time
-                            #Epoch unix time stamp from dstat - UST
+                            #Epoch unix time stamp from dstat in UST
                             #Note "\n" tag only valid if epoch time is at the end of line
                             elif "\n" in i:
                                 trim = i.split("\n") #getLine
@@ -107,6 +77,6 @@ def log_parser(logName):
         return log_file(logDict)
 
 if __name__ == "__main__":
-    logObj = log_parser("/home/jguiang/Metis/scripts/log_files/testfile0.txt")
+    logObj = log_parser("/home/jguiang/ProjectMetis/log_files/CMSSWTask_SinglePhoton_Run2017B-PromptReco-v1_MINIAOD_CMS4_V00-00-03/logs/std_logs/1e.1090614.0.out")
     print(logObj["epoch"])
     print(logObj.keys())
--- Plotter.py
+++ Plotter.py
@@ -5,9 +5,41 @@ import numpy as np
 #Other imports
 import LogParser as lp
 
+#Log File Functions:
+#Imports os, retrieves .out files -> ONLY FOR DEBUGGING PURPOSES
+def get_log_files():
+    import os
+    
+    os.chdir("log_files")
+
+    fPile = []
+    dirLst = os.listdir(os.curdir)
+    
+    for d in dirLst:
+        if d == "plots":
+            continue
+        else:
+            newdir = os.listdir(d + "/logs/std_logs") 
+            for f in newdir:
+                if f.endswith(".out"):
+                    fPile.append(os.path.dirname(os.path.abspath(f))+"/"+d+"/logs/std_logs/"+f)
+                
+    return fPile
+
+#Takes list of log file paths, outputs list of log file dictionaries -> {"key":[list of values]}
+def parse_log_files(fPile):
+    logObjPile = {}
+    counter = 0
+
+    for fpath in fPile:
+        logObjPile["logfile{}".format(counter)] = lp.log_parser(fpath)
+        counter += 1
+
+    return logObjPile
+
 #Plotting Functions
 def plot_1DHist(**kwargs):
-    plt.hist(kwargs.get("data", 0), kwargs.get("bins", 20))
+    plt.hist(kwargs.get("data", 0), kwargs.get("bins", 200))
     plt.title(kwargs.get("title", "1D Histogram"))
     plt.xlabel(kwargs.get("xlabel",""))
     plt.ylabel(kwargs.get("ylabel",""))
@@ -16,36 +48,74 @@ def plot_1DHist(**kwargs):
     
     return
 
-#Log File Functions:
-#Takes list of log file paths, outputs list of log file dictionaries -> {"key":[list of values]}
-def parse_log_files(filePile):
-    logObjPile = {}
-    counter = 0
+def plot_2DHeat(**kwargs):
+    #Import Colors
+    from matplotlib.colors import LogNorm    
 
-    for fpath in filePile:
-        logObjPile["logfile{}".format(counter)] = lp.log_parser(fpath)
-        counter += 1
+    #Create Heatmap
+    plt.hist2d(kwargs.get("xdata", 0), kwargs.get("ydata", 0), bins = kwargs.get("bins", 100), norm = LogNorm())
 
-    return logObjPile
+    #Plot Heatmap
+    plt.colorbar()
+    plt.title(kwargs.get("title", "2D Heatmap"))
+    plt.xlabel(kwargs.get("xlabel",""))
+    plt.ylabel(kwargs.get("ylabel",""))
+
+    plt.show()
+    
+    return
 
 #Plots 1D Histogram for a single logfile and key
 def plot_log_1DHist(logname, key):
     plot_1DHist(data = logname[key], bins = 20, xlabel = key)
+    
+    return
 
 #Plots 1D Histogram for all logfiles for a particular key
-def plot_all(logObjPile, key):
+def plot_all_1DHist(logObjPile, key):
     lst = []
     for log in logObjPile:
-        for pnt in logObjPile[log][key]:
-            lst.append(float(pnt))
+        try:
+            for pnt in logObjPile[log][key]:
+                lst.append(float(pnt))
+        except KeyError:
+            pass
+
     plot_1DHist(data = lst, bins = 20, xlabel = key)
+
+    return
+
+#Plots KEY data vs time as a heatmap plot
+def plot_key_vs_t_Heat(key):
+    timeLst = []
+    dataLst = []
+    for log in logObjPile:
+        try:
+            tstart = logObjPile[log]['epoch'][0]
+            for pnt in logObjPile[log][key]:
+                timeLst.append(float(pnt))
+            for pnt in logObjPile[log]['epoch']:
+                dataLst.append(float(pnt) - float(tstart))
+        except (KeyError, IndexError) as error:
+            pass
+
+    if len(timeLst) == len(dataLst):
+        plot_2DHeat(xdata = timeLst, ydata = dataLst, xlabel = 'time', ylabel = key)
+    else:
+        print('Error: x and y data sets are not the same size')
+
+    return
 
 #Debugging operations
 if __name__ == "__main__":
-    filePile = ["/home/jguiang/Metis/scripts/log_files/testfile1.txt","/home/jguiang/Metis/scripts/log_files/testfile0.txt"]
+    filePile = get_log_files()
     logObjPile = parse_log_files(filePile)
 
-    #plot_log_1DHist(logObjPile["logfile0"], "usr")
+    plot_key_vs_t_Heat("usr")
+    
+    #plot_key_vs_t_Heat("sys")
+
+    #plot_log_1DHist(logObjPile["logfile327"], "usr")
 
     #plot_all(logObjPile, "usr")
 
